9122e9192bd0e03c976fb24f777985bd904c4ba0
Ziyi Zhang <zhangziyibupt@gmail.com>
2022-01-05 15:35:15 -0800

    Add the HTTP network API sample app

---
+++ SimpleAPICallUsingGson/app/src/main/java/com/simpleapicallusinggson/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.simpleapicallusinggson
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.os.AsyncTask
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CallAPILoginAsyncTask("Panjutorials", "123456").execute()
+    }
+
+    /**
+     * “A nested class marked as inner can access the members of its outer class.
+     * Inner classes carry a reference to an object of an outer class:”
+     * source: https://kotlinlang.org/docs/reference/nested-classes.html
+     *
+     * This is the background class is used to execute background task.
+     *
+     * For Background we have used the AsyncTask
+     *
+     * Asynctask : Creates a new asynchronous task. This constructor must be invoked on the UI thread.
+     */
+    @SuppressLint("StaticFieldLeak")
+    private inner class CallAPILoginAsyncTask(val username: String, val password: String) :
+        AsyncTask<Any, Void, String>() {
+
+        private lateinit var customProgressDialog: Dialog
+
+        /**
+         * This function is for the task which we wants to perform before background execution.
+         * Here we have shown the progress dialog to user that UI is not freeze but executing something in background.
+         */
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any): String {
+            var result: String
+
+            // https://developer.android.com/reference/java/net/HttpURLConnection
+            var connection: HttpURLConnection? = null
+            try {
+                val url = URL("http://www.mocky.io/v2/5e3826143100006a00d37ffa")
+                connection = url.openConnection() as HttpURLConnection
+                connection.doOutput = true
+                connection.doInput = true
+                connection.instanceFollowRedirects = false
+                connection.requestMethod = "POST"
+                connection.setRequestProperty("Content-Type", "application/json")
+                connection.setRequestProperty("charset", "utf-8")
+                connection.setRequestProperty("Accept", "application/json")
+                connection.useCaches = false
+
+                // Creates a new data output stream to write data to the specified underlying output stream.
+                val writeDataOutputStream = DataOutputStream(connection.outputStream)
+
+                // Create JSONObject Request
+                val jsonRequest = JSONObject()
+                jsonRequest.put("username", username)
+                jsonRequest.put("password", password)
+
+                // Writes out the string to the underlying output stream as a sequence of bytes.
+                writeDataOutputStream.writeBytes(jsonRequest.toString())
+                writeDataOutputStream.flush() // Flushes this data output stream.
+                writeDataOutputStream.close() // Closes this output stream and releases any system resources associated with the stream
+
+                // Gets the status code from an HTTP response message.
+                val httpResult: Int = connection.responseCode
+                if (httpResult == HttpURLConnection.HTTP_OK) {
+                    val inputStream = connection.inputStream
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val sb = StringBuilder()
+                    var line: String?
+                    try {
+                        /**
+                         * Reads a line of text.  A line is considered to be terminated by any one
+                         * of a line feed ('\n'), a carriage return ('\r'), or a carriage return
+                         * followed immediately by a linefeed.
+                         */
+                        while (reader.readLine().also { line = it } != null) {
+                            sb.append(line + "\n")
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        try {
+                            // Closes this input stream and releases any system resources associated with the stream.
+                            inputStream.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    result = sb.toString()
+                } else {
+                    result = connection.responseMessage
+                }
+
+            } catch (e: SocketTimeoutException) {
+                result = "Connection Timeout"
+            } catch (e: Exception) {
+                result = "Error : " + e.message
+            } finally {
+                connection?.disconnect()
+            }
+
+            // You can notify with your result to onPostExecute.
+            return result
+        }
+
+        // This function will be executed after the background execution is completed.
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+
+            Log.i("JSON Response Result", result)
+
+            // Map the json response with the Data Class using GSON.
+            val responseData = Gson().fromJson(result, ResponseData::class.java)
+            Log.i("Message", responseData.message)
+            Log.i("User Id", "${responseData.user_id}")
+            Log.i("Name", responseData.name)
+            Log.i("Email", responseData.email)
+            Log.i("Mobile", "${responseData.mobile}")
+
+            // Profile Details
+            Log.i("Is Profile Completed", "${responseData.profile_details.is_profile_completed}")
+            Log.i("Rating", "${responseData.profile_details.rating}")
+
+            // Data List Details.
+            Log.i("Data List Size", "${responseData.data_list.size}")
+
+            for (item in responseData.data_list.indices) {
+                Log.i("Value $item", "${responseData.data_list[item]}")
+
+                Log.i("ID", "${responseData.data_list[item].id}")
+                Log.i("Value", "${responseData.data_list[item].value}")
+            }
+            Toast.makeText(this@MainActivity, responseData.message, Toast.LENGTH_SHORT).show()
+        }
+
+        private fun showProgressDialog() {
+            customProgressDialog = Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            customProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog() {
+            customProgressDialog.dismiss()
+        }
+    }
+}
